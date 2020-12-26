33cf46305b454b2ff6b4dac36e925e70d6980d41
oguzkr <oguzkaratoruk@yandex.com>
2020-12-26 18:06:50 +0300

    Initial Commit

---
+++ MarvelCharacters/Helpers/Settings.swift
@@ -0,0 +1,25 @@
+//
+//  Settings.swift
+//  MarvelCharacters
+//
+//  Created by Oğuz Karatoruk on 26.12.2020.
+//
+
+import Foundation
+import UIKit
+import CryptoSwift
+
+struct Settings {
+    
+    static let publicKey:String = "b426daffa89836d275893ff671357411"
+    static let privateKey:String = "2640814ba22282f4f38b491ee2b7365ac6be519c"
+    static let ts = Date().timeIntervalSince1970.description
+    static let hash = "\(ts)\(privateKey)\(publicKey)".md5()
+    static let characterRequestURL:String = "https://gateway.marvel.com/v1/public/characters?ts=\(ts)&apikey=\(publicKey)&hash=\(hash)"
+    
+    
+    static let darkColor = UIColor(named: "darkColor")
+    static let marvelRedColor = UIColor(named: "marvelRedColor")
+    static let marvelRedLightColor = UIColor(named: "marvelRedLightColor")
+
+}
---
+++ MarvelCharacters/Network/Requests/requestGetCharacters.swift
@@ -0,0 +1,40 @@
+//
+//  requestGetCharacters.swift
+//  MarvelCharacters
+//
+//  Created by Oğuz Karatoruk on 26.12.2020.
+//
+
+import Foundation
+import Alamofire
+import SVProgressHUD
+
+class networkManager {
+    
+    //var rovers = [Photos]()
+   // var url = Settings.PUBLIC_API_KEY
+
+//    func getRoverData(tab:Int, page:Int, completed: @escaping () -> ()){
+//        SVProgressHUD.show()
+//        
+//        AF.request(url).responseData { response in
+//            switch response.result {
+//            case .failure(let error):
+//                print(error)
+//            case .success(let data):
+//                do {
+//                    let roverData = try JSONDecoder().decode(Rovers.self, from: data)
+//                    self.rovers = roverData.photos
+//                    DispatchQueue.main.async {
+//                        SVProgressHUD.dismiss()
+//                        completed()
+//                    }
+//                } catch let error {
+//                    print(error)
+//                    SVProgressHUD.dismiss()
+//                }
+//            }
+//        }.resume()
+//    }
+    
+}
