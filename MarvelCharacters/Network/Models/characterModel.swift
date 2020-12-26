bacb32c808ecfc376a317a7a5950dbcc5539df83
oguzkr <oguzkaratoruk@yandex.com>
2020-12-26 19:48:56 +0300

    get character data with alamofire

---
+++ MarvelCharacters/Network/Models/characterModel.swift
@@ -0,0 +1,103 @@
+//
+//  characterModel.swift
+//  MarvelCharacters
+//
+//  Created by Oğuz Karatoruk on 26.12.2020.
+//
+
+import Foundation
+
+// MARK: - Characters
+struct Characters: Codable {
+    let code: Int
+    let data: DataClass
+}
+
+// MARK: - DataClass
+struct DataClass: Codable {
+    let results: [Result]
+}
+
+// MARK: - Result
+struct Result: Codable {
+    let id: Int
+    let name: String
+    let resultDescription: String?
+    //let modified: Date
+    let thumbnail: Thumbnail
+    let resourceURI: String
+    let comics, series: Comics
+    let stories: Stories
+    let events: Comics
+    let urls: [URLElement]
+
+    enum CodingKeys: String, CodingKey {
+        case id, name
+        case resultDescription = "description"
+        case thumbnail, resourceURI, comics, series, stories, events, urls
+        //case modified
+    }
+}
+
+// MARK: - Comics
+struct Comics: Codable {
+    let available: Int
+    let collectionURI: String
+    let items: [ComicsItem]
+    let returned: Int
+}
+
+// MARK: - ComicsItem
+struct ComicsItem: Codable {
+    let resourceURI: String
+    let name: String
+}
+
+// MARK: - Stories
+struct Stories: Codable {
+    let available: Int
+    let collectionURI: String
+    let items: [StoriesItem]
+    let returned: Int
+}
+
+// MARK: - StoriesItem
+struct StoriesItem: Codable {
+    let resourceURI: String
+    let name: String
+    let type: ItemType
+}
+
+enum ItemType: String, Codable {
+    case cover = "cover"
+    case empty = ""
+    case interiorStory = "interiorStory"
+}
+
+// MARK: - Thumbnail
+struct Thumbnail: Codable {
+    let path: String
+    let thumbnailExtension: Extension
+
+    enum CodingKeys: String, CodingKey {
+        case path
+        case thumbnailExtension = "extension"
+    }
+}
+
+enum Extension: String, Codable {
+    case gif = "gif"
+    case jpg = "jpg"
+}
+
+// MARK: - URLElement
+struct URLElement: Codable {
+    let type: URLType
+    let url: String
+}
+
+enum URLType: String, Codable {
+    case comiclink = "comiclink"
+    case detail = "detail"
+    case wiki = "wiki"
+}
--- MarvelCharacters/Viewcontrollers/ViewController.swift
+++ MarvelCharacters/Viewcontrollers/ViewController.swift
@@ -6,12 +6,27 @@
 //
 
 import UIKit
+import Alamofire
+import SDWebImage
+import SwiftGifOrigin
+import SVProgressHUD
 
 class ViewController: UIViewController {
-
+    
+    var network: networkManager = networkManager()
+    var characters = [Result]()
+    var scrollcontrol = true
+    var cameraFilterOptions = Array<String>()
+    let margin: CGFloat = 10
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        network.getCharacters() {
+            self.characters = self.network.characters
+            //self.collectionView.reloadData()
+            print(self.characters[0].name)
+        }
     }
 
 
