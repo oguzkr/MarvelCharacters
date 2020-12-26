3e21111c1f3d311495c1ff3163c3492d59ec2db4
oguzkr <oguzkaratoruk@yandex.com>
2020-12-26 18:06:50 +0300

    Initial Commit

---
+++ MarvelCharacters/Helpers/Settings.swift
@@ -0,0 +1,25 @@
+//
+//  Settings.swift
+//  MarvelCharacters
+//
+//  Created by Oğuz Karatoruk on 26.12.2020.
+//
+
+import Foundation
+import UIKit
+import CryptoSwift
+
+struct Settings {
+    
+    static let publicKey:String = "b426daffa89836d275893ff671357411"
+    static let privateKey:String = "<REMOVED_MARVEL_PRIVATE_KEY>"
+    static let ts = Date().timeIntervalSince1970.description
+    static let hash = "\(ts)\(privateKey)\(publicKey)".md5()
+    static let characterRequestURL:String = "https://gateway.marvel.com/v1/public/characters?ts=\(ts)&apikey=\(publicKey)&hash=\(hash)"
+    
+    
+    static let darkColor = UIColor(named: "darkColor")
+    static let marvelRedColor = UIColor(named: "marvelRedColor")
+    static let marvelRedLightColor = UIColor(named: "marvelRedLightColor")
+
+}
